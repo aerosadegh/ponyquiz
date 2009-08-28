ed23872005d49336140bd609beb7e7c03453c1c1
John Leith <John Leith@Hadassah>
2009-08-27 20:49:03 -0600

    removing fluff

--- ponyquiz/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-from views import *
-
-urlpatterns = patterns('',
-#    url(r'^(\w+)/', quiz_view, name="quiz-view"),
-#    url(r'^/$', quiz_index, name="quiz-index"),
-)
